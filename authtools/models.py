9b48a8e6cb2d9dabc53b55733adcb8891b627c57
George Lee <cgeorgelee@gmail.com>
2022-02-22 15:54:19 -0600

    Removing support for python2 which causes import error in django 3

--- authtools/models.py
+++ authtools/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.core.mail import send_mail
 from django.db import models
 from django.utils import timezone
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 
